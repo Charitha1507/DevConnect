69aa09d1dc6807c867816e24ea8737591cc6a42f
Charitha1507 <saic06562@gmail.com>
2025-07-22 14:39:39 +0530

    Update app.js

--- Dev_tinder/src/app.js
+++ Dev_tinder/src/app.js
@@ -14,7 +14,7 @@ const cors=require('cors');
 const http = require('http');
 const initializeSocket = require('./utils/socket');
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
 }));
 app.use(express.json());
